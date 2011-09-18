12036d7fb80f92e10a18f226be3b2f0315354812
Christian Zenker <christian.zenker@599media.de>
2011-09-18 17:21:09 +0200

    [+FEATURE] EventIndex_Count Widget

---
+++ Classes/ViewHelpers/Widget/EventIndex/Controller/CountController.php
@@ -0,0 +1,119 @@
+<?php
+
+/*                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU Lesser General Public License as published by the *
+ * Free Software Foundation, either version 3 of the License, or (at your *
+ * option) any later version.                                             *
+ *                                                                        *
+ * This script is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHAN-    *
+ * TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser       *
+ * General Public License for more details.                               *
+ *                                                                        *
+ * You should have received a copy of the GNU Lesser General Public       *
+ * License along with the script.                                         *
+ * If not, see http://www.gnu.org/licenses/lgpl.html                      *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+/**
+ * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License, version 3 or later
+ */
+class Tx_CzSimpleCal_ViewHelpers_Widget_EventIndex_Controller_CountController extends Tx_Fluid_Core_Widget_AbstractWidgetController {
+
+	/**
+	 * @param Tx_CzSimpleCal_Domain_Repository_EventIndexRepository $eventIndexRepository
+	 */
+	public function injectEventIndexRepository(Tx_CzSimpleCal_Domain_Repository_EventIndexRepository $eventIndexRepository) {
+		$this->eventIndexRepository = $eventIndexRepository;
+	}
+	
+	/**
+	 * the action settings to use for fetching the events
+	 * 
+	 * @var array
+	 */
+	protected $actionSettings = array();
+	
+	/**
+	 * @return void
+	 */
+	public function initializeAction() {
+		foreach(array('maxEvents', 'order', 'orderBy', 'includeStartedEvents', 'excludeOverlongEvents', 'filter', 'groupBy') as $argumentName) {
+			if($this->widgetConfiguration->offsetExists($argumentName)) {
+				$this->actionSettings[$argumentName] = $this->widgetConfiguration[$argumentName];
+			}
+		}
+		foreach(array('startDate', 'endDate') as $argumentName) {
+			if($this->widgetConfiguration->offsetExists($argumentName)) {
+				$this->actionSettings[$argumentName] = $this->normalizeArgumentToTimestamp($this->widgetConfiguration[$argumentName]);
+			}
+		}
+	}
+	
+	/**
+	 * normalizes anything that describes a time
+	 * and sets it to be a timestamp
+	 * 
+	 * @param mixed $value
+	 * @return void
+	 */
+	protected function normalizeArgumentToTimestamp($value) {
+		if(empty($value)) {
+			return;
+		} elseif(is_numeric($value)) {
+			return t3lib_div::intInRange($this->argumentName, 0);
+		} elseif(is_string($value)) {
+			return Tx_CzSimpleCal_Utility_StrToTime::strtotime($value);
+		} elseif($value instanceof DateTime) {
+			return intval($value->format('U'));
+		}
+		return;
+	}
+	
+	/**
+	 * @return void
+	 */
+	public function indexAction() {
+		$this->view->assign(
+			'data',
+			$this->eventIndexRepository->countAllWithSettings($this->actionSettings)
+		);
+		$this->view->assign('actionSettings', $this->actionSettings);
+	}
+	
+	/**
+	 * Allows the widget template root path to be overriden via the framework configuration,
+	 * e.g. plugin.tx_extension.view.widget.<WidgetViewHelperClassName>.templateRootPath
+	 * 
+	 * This implementation was suggested in the ticket below, but was not yet
+	 * integrated in the Extbase core.
+	 * 
+	 * @todo remove, override or modify this method as soon as this feature is in extbase
+	 * @param Tx_Extbase_MVC_View_ViewInterface $view
+	 * @see Classes/Core/Widget/Tx_Fluid_Core_Widget_AbstractWidgetController::setViewConfiguration()
+	 * @see http://forge.typo3.org/issues/10823
+	 */
+	protected function setViewConfiguration(Tx_Extbase_MVC_View_ViewInterface $view) {
+		$extbaseFrameworkConfiguration = $this->configurationManager->getConfiguration(Tx_Extbase_Configuration_ConfigurationManagerInterface::CONFIGURATION_TYPE_FRAMEWORK);
+		$widgetViewHelperClassName = $this->request->getWidgetContext()->getWidgetViewHelperClassName();
+		
+		if (isset($extbaseFrameworkConfiguration['view']['widget'][$widgetViewHelperClassName]['templateRootPath'])
+				&& strlen($extbaseFrameworkConfiguration['view']['widget'][$widgetViewHelperClassName]['templateRootPath']) > 0
+				&& method_exists($view, 'setTemplateRootPath')) {
+			
+			if($this->widgetConfiguration->hasArgument('templateFilePath')) {
+				$view->setTemplatePathAndFilename(t3lib_div::getFileAbsFileName($extbaseFrameworkConfiguration['view']['widget'][$widgetViewHelperClassName]['templateRootPath']).$this->widgetConfiguration['templateFilePath']);
+			} else {
+				$view->setTemplateRootPath(t3lib_div::getFileAbsFileName($extbaseFrameworkConfiguration['view']['widget'][$widgetViewHelperClassName]['templateRootPath']));
+			}
+			
+		} elseif($this->widgetConfiguration->hasArgument('templateFilePath')) {
+			$view->setTemplatePathAndFilename(t3lib_div::getFileAbsFileName($this->widgetConfiguration['templateFilePath']));
+		}
+	}
+}
+
+?>
---
+++ Classes/ViewHelpers/Widget/EventIndex/CountViewHelper.php
@@ -0,0 +1,86 @@
+<?php
+
+/*                                                                        *
+ * This script belongs to the FLOW3 package "Fluid".                      *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU Lesser General Public License as published by the *
+ * Free Software Foundation, either version 3 of the License, or (at your *
+ * option) any later version.                                             *
+ *                                                                        *
+ * This script is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHAN-    *
+ * TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser       *
+ * General Public License for more details.                               *
+ *                                                                        *
+ * You should have received a copy of the GNU Lesser General Public       *
+ * License along with the script.                                         *
+ * If not, see http://www.gnu.org/licenses/lgpl.html                      *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+/**
+ * This view helper allows you to embed a list of events into a different action
+ * 
+ * The arguments are exacly the same as in EventIndex's action "list". So you
+ * can transfer all the flexibility you have there to this Widget. For ease of use 
+ * all basic properties where transformed into attributes of the ViewHelper.
+ * 
+ * <example>
+ *   <code>
+ *     <cal:widget.eventIndex.list startDate="now" maxEvents="3" />
+ *   </code>
+ *   
+ *   will show you the next 3 events from now on.
+ * </example>
+ *
+ * @author Christian Zenker <christian.zenker@599media.de>
+ * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License, version 3 or later
+ */
+class Tx_CzSimpleCal_ViewHelpers_Widget_EventIndex_CountViewHelper extends Tx_Fluid_Core_Widget_AbstractWidgetViewHelper {
+
+	/**
+	 * @var Tx_CzSimpleCal_ViewHelpers_Widget_EventIndex_Controller_CountController
+	 */
+	protected $controller;
+
+	/**
+	 * @param Tx_CzSimpleCal_ViewHelpers_Widget_EventIndex_Controller_CountController $controller
+	 * @return void
+	 */
+	public function injectController(Tx_CzSimpleCal_ViewHelpers_Widget_EventIndex_Controller_CountController $controller) {
+		$this->controller = $controller;
+	}
+
+	
+	/**
+	 * Initialize all arguments. You need to override this method and call
+	 * $this->registerArgument(...) inside this method, to register all your arguments.
+	 *
+	 * @return void
+	 */
+	public function initializeArguments() {
+		$this->registerArgument('startDate', 'mixed', 'startDate', false, null);
+		$this->registerArgument('endDate', 'mixed', 'endDate', false, null);
+		$this->registerArgument('maxEvents', 'integer', 'maxEvents', false, 3);
+		$this->registerArgument('order', 'string', 'order', false, 'asc');
+		$this->registerArgument('orderBy', 'string', 'orderBy', false, 'startDate');
+		$this->registerArgument('includeStartedEvents', 'integer', 'includeStartedEvents', false, null);
+		$this->registerArgument('excludeOverlongEvents', 'integer', 'excludeOverlongEvents', false, null);
+		$this->registerArgument('filter', 'array', 'filter', false, null);
+		$this->registerArgument('groupBy', 'string', 'groupBy', false, null);
+		
+		
+		$this->registerArgument('templateFilePath', 'string', 'template file path', false, null);
+	}
+	
+	/**
+	 * @return string
+	 */
+	public function render() {
+		return $this->initiateSubRequest();
+	}
+}
+
+?>
