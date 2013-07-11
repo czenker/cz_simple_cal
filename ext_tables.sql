9fe691b0d37fa14b8da60bd41d4beca78e9a0865
Christian Zenker <christian.zenker@aoemedia.de>
2013-07-11 15:03:10 +0200

    add t3ver_move_id to database tables to prevent exceptions in workspace preview
    Note that this does not mean, the extension is fully working with
    workspaces. Extbase has some bugs right now so you are not recommended
    to use events inside workspaces.

--- ext_tables.sql
+++ ext_tables.sql
@@ -60,6 +60,7 @@ CREATE TABLE tx_czsimplecal_domain_model_event (
 	t3ver_stage tinyint(4) DEFAULT '0' NOT NULL,
 	t3ver_count int(11) DEFAULT '0' NOT NULL,
 	t3ver_tstamp int(11) DEFAULT '0' NOT NULL,
+	t3ver_move_id int(11) DEFAULT '0' NOT NULL,
 	t3_origuid int(11) DEFAULT '0' NOT NULL,
 
 	sys_language_uid int(11) DEFAULT '0' NOT NULL,
@@ -198,6 +199,7 @@ CREATE TABLE tx_czsimplecal_domain_model_category (
 	t3ver_stage tinyint(4) DEFAULT '0' NOT NULL,
 	t3ver_count int(11) DEFAULT '0' NOT NULL,
 	t3ver_tstamp int(11) DEFAULT '0' NOT NULL,
+	t3ver_move_id int(11) DEFAULT '0' NOT NULL,
 	t3_origuid int(11) DEFAULT '0' NOT NULL,
 
 	sys_language_uid int(11) DEFAULT '0' NOT NULL,
@@ -234,6 +236,7 @@ CREATE TABLE tx_czsimplecal_domain_model_address (
 	t3ver_stage tinyint(4) DEFAULT '0' NOT NULL,
 	t3ver_count int(11) DEFAULT '0' NOT NULL,
 	t3ver_tstamp int(11) DEFAULT '0' NOT NULL,
+	t3ver_move_id int(11) DEFAULT '0' NOT NULL,
 	t3_origuid int(11) DEFAULT '0' NOT NULL,
 
 	sys_language_uid int(11) DEFAULT '0' NOT NULL,
